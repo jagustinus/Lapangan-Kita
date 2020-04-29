f0bcb22f1f71b3be3a2259e04c89aae99b68278f
jeems-uc <jagustinus@student.ciputra.ac.id>
2020-04-29 09:36:46 +0700

    Initial commit

---
+++ app/src/main/java/com/lapangankita/lapangankita/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lapangankita.lapangankita;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
